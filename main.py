6bccc0b9fb9edae74263ab0030e80683005e093d
Aleksei Shpitalev <aleksei.shpitalev@gmail.com>
2022-08-16 21:49:26 +0700

    implement bobber find with yolov5 and grabcat

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import win32gui
 import ctypes
 import win32api
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import cv2 as cv
 
 
 def get_wow_hwnd():
@@ -40,3 +45,223 @@ def dpi_to_scale_ratio(dpi):
     if len(dpi) != 2 or dpi[0] != dpi[1]:
         raise RuntimeError(f'non conformant DPI:{dpi[0]}x{dpi[1]}')
     return dpi[0] / STANDARD_DPI
+
+def plot_images(imgs, images_per_row = 3):
+    """Plot a series of images"""
+    plt.gray()
+    plt.rcParams.update({"figure.facecolor":  (0.0, 0.0, 0.0, 0.5)})
+    rows = math.ceil(len(imgs)/images_per_row)
+    f = plt.figure(figsize=(38, rows * 7))
+    for k, l in enumerate(imgs):
+        f.add_subplot(rows, images_per_row, k + 1)
+        plt.imshow(l)
+    plt.show()
+
+
+def dilate_erode(img, ds = 5):
+    """Combined dilate-erode filter"""
+    element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2 * ds + 1, 2 * ds + 1), (ds, ds))
+    im = img
+    im = cv.dilate(im, element)
+    im = cv.erode(im, element)
+    return im
+
+
+def dilate(img, ds = 5):
+    """Combined dilate-erode filter"""
+    element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2 * ds + 1, 2 * ds + 1), (ds, ds))
+    im = img
+    im = cv.dilate(im, element)
+    return im
+
+
+def erode(img, ds = 5):
+    """Combined dilate-erode filter"""
+    element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2 * ds + 1, 2 * ds + 1), (ds, ds))
+    im = img
+    im = cv.erode(im, element)
+    return im
+
+
+def fill_gaps(mask):
+    imgs = []
+    mask_h, mask_w = mask.shape
+    inv = np.invert(mask)
+    imgs.append(inv)
+    n, l, s, c = cv.connectedComponentsWithStats(inv, connectivity=8)
+    label_idx = next((i for i, (_, _, w, h, _) in enumerate(s) \
+        if i != 0 and w == mask_w and h == mask_h), None)
+    label = (l == label_idx).astype(np.uint8)
+    label *= 255
+    filled = np.invert(label)
+    imgs.append(filled)
+    return filled, imgs
+
+
+def filter_connected_components(mask, predicate):
+    _, labels, stats, centroids = cv.connectedComponentsWithStats(mask, connectivity=8)
+    return list(i for i, st in enumerate(stats) \
+        if i != 0 and predicate(i, st, labels, centroids)), labels, centroids
+
+def find_threshold(ch0, th=0.01):
+    x = 64
+    amount = 1.0
+    w, h = ch0.shape
+    while amount > th:
+        mask = (ch0 > x).astype(np.uint8)
+        #mask = dilate_erode(mask, 3)
+        cnt = np.count_nonzero(mask > 0)
+        amount = cnt / (w*h)
+        x += 3
+    mask *= 255
+    return mask
+
+def solution(img, t, plot=False):
+    hsv = cv.cvtColor(img, cv.COLOR_RGB2LUV)
+    hue = hsv[:,:,0]
+
+    def in_range(x, a, b):
+        return a < x and x < b
+
+    sol = False
+    if t == None:
+        t = 128
+        while not sol or t > 250:
+            thresh = (hue > t).astype(np.uint8)
+            nb_components, output, stats, centroids = cv.connectedComponentsWithStats(thresh, connectivity=4)
+            for i in range(1, nb_components):
+                q = ((output==i).astype(np.uint8))
+                x, y, w, h, a = stats[i]
+                if in_range(w/h, 0.79, 1.26) and in_range(a, 9, 25) and in_range(a / (w*h), 0.5, 1.0):
+                    #print(t)
+                    sol = True
+                    # x = int(centroids[i][0])
+                    # y = int(centroids[i][1])
+                    img = cv.bitwise_and(img, img, mask=q)
+            if not sol:
+                t += 2
+    else:
+        #print('qwe')
+        thresh = (hue > t).astype(np.uint8)
+        nb_components, output, stats, centroids = cv.connectedComponentsWithStats(thresh, connectivity=4)
+        for i in range(1, nb_components):
+            q = ((output==i).astype(np.uint8))
+            x, y, w, h, a = stats[i]
+            if in_range(w/h, 0.79, 1.26) and in_range(a, 9, 25) and in_range(a / (w*h), 0.5, 1.0):
+                sol = True
+                # x = int(centroids[i][0])
+                # y = int(centroids[i][1])
+                img = cv.bitwise_and(img, img, mask=q)
+
+    if plot:
+        plot_images([img, hue, thresh])
+    if sol:
+        return t, x, y, w, h, a
+    else:
+        return None
+
+# def sol1(img):
+#     img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+#     img = dilate_erode(img, 5)
+#     red1 = (0, 0, 0)
+#     red2 = (9, 255, 255)
+#     imgs = []
+#     hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
+#     m_red = cv.inRange(hsv, red1, red2)
+#     nb_components, output, stats, centroids = cv.connectedComponentsWithStats(m_red, connectivity=4)
+#     m = max(zip(stats[1:], range(1,nb_components)), key=lambda x: x[0][cv.CC_STAT_AREA])
+#     max_lab = (output == m[1]).astype(np.uint8)
+#     max_lab *= 255
+#     imgs += [img, m_red, cv.bitwise_and(img, img, mask=max_lab)]
+#     imgs.append(max_lab)
+#     return max_lab, tuple(map(int, centroids[m[1]])), m[0][cv.CC_STAT_AREA], imgs
+
+
+# def sol2(img):
+#     img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+#     img = dilate_erode(img, 3)
+#     imgs = []
+#     hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
+#     hue = hsv[:,:,0]
+#     sat = hsv[:,:,1]
+#     m_red = ((hue < 14) * (sat > 70)).astype(np.uint8)
+#     m_red *= 255
+#     # imgs += [img, hsv, m_red, cv.bitwise_and(img, img, mask=m_red)]
+#     nb_components, output, stats, centroids = cv.connectedComponentsWithStats(m_red, connectivity=4)
+#     if nb_components > 1:
+#         m = max(zip(stats[1:], range(1,nb_components)), key=lambda x: x[0][cv.CC_STAT_AREA])
+#         max_lab = (output == m[1]).astype(np.uint8)
+#         max_lab *= 255
+#         # imgs.append(max_lab)
+#         return max_lab, tuple(map(int, centroids[m[1]])), m[0][cv.CC_STAT_AREA], imgs
+#     else:
+#         return None, None, None, imgs
+
+
+def sol3(img):
+    img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+    
+    imgs = []
+    hsv = cv.cvtColor(img, cv.COLOR_RGB2Luv)
+    hue = hsv[:,:,0]
+    #sat = hsv[:,:,1]
+    #m_red = (((108 < hue) * (hue < 120)) * (80 > sat)).astype(np.uint8)
+    m_red = (hue > 167).astype(np.uint8)
+    m_red *= 255
+    #m_red = dilate_erode(m_red, 1)
+    # imgs += [img, hsv, m_red, cv.bitwise_and(img, img, mask=m_red)]
+    nb_components, output, stats, centroids = cv.connectedComponentsWithStats(m_red, connectivity=4)
+    if nb_components > 1:
+        m = max(zip(stats[1:], range(1,nb_components)), key=lambda x: x[0][cv.CC_STAT_AREA])
+        max_lab = (output == m[1]).astype(np.uint8)
+        max_lab *= 255
+        # imgs.append(max_lab)
+        return max_lab, tuple(map(int, centroids[m[1]])), m[0][cv.CC_STAT_AREA], imgs
+    else:
+        return None, None, None, imgs
+
+
+def sol(img):
+    mask = np.zeros(img.shape[:2],np.uint8)
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+    h,w,_ = img.shape
+    rect = (2,2, w-4, h-4)
+    cv.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv.GC_INIT_WITH_RECT)
+    mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+    #img = img*mask2[:,:,np.newaxis]
+    # img = dilate_erode(img, 3)
+    # img = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+    # img = img[:,:,0]
+    # img = (img > 96).astype(np.uint8)
+    # img *= 255
+
+    img = np.ones(img.shape[:2], np.uint8)*mask2[:,:]
+    img *= 255
+    # img_ = img.copy()
+    # img = dilate_erode(img, 5)
+    # print(img.shape)
+
+    # nb_components, output, stats, centroids = cv.connectedComponentsWithStats(img, connectivity=4)
+    # if nb_components > 1:
+    #     m = max(zip(stats[1:], range(1,nb_components)), key=lambda x: x[0][cv.CC_STAT_AREA])
+    #     x, y = tuple(map(int, centroids[m[1]]))
+    #     max_lab = (output == m[1]).astype(np.uint8)
+    #     max_lab *= 255
+    
+    a = np.count_nonzero(img)
+    if a < 100:
+        return None, None, None, None
+    center = [ np.average(indices) for indices in np.where(img >= 255) ]
+    x, y = map(int, center)
+
+    #img[x][y] = 0
+    #print(img.min(), img.max())
+
+    img1 = np.zeros((*img.shape[:2], 3), dtype=np.uint8)
+    img1[:,:,0] = img[:,:]
+    img1[:,:,1] = img[:,:]
+    img1[:,:,2] = img[:,:]
+    img1[x][y] = (255,0,0)
+    # img[x][y] = 0
+    return x, y, a, img
--- overlay.py
+++ overlay.py
@@ -1,18 +1,21 @@
 import sys
 from PyQt5 import QtGui, QtCore, uic
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication, QRubberBand, QLabel
-from PyQt5.QtCore import QTimer, QPoint, QRect, QSize, QRectF
-from PyQt5.QtGui import QImage, QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication, QRubberBand, QLabel, QStackedLayout, QVBoxLayout, QWidget, QSpacerItem
+from PyQt5.QtCore import QTimer, QPoint, QRect, QSize
+from PyQt5.QtGui import QImage, QPainter, QColor, QVector2D, QPixmap
 from main import *
 import win32gui
 import ctypes
 import ahk
+import traceback
 from enum import Enum
 from datetime import *
 from time import time
 import numpy as np
 import cv2
+from yolov5.minimal import BobberDetector
+from PyQt5.QtCore import pyqtSlot, QRunnable, QThreadPool, pyqtSignal, QObject, QMutexLocker, QMutex
 
 def millis_now():
     return int(time() * 1000)
@@ -21,73 +24,156 @@ class WindowState(Enum):
     NORMAL = 'normal'
     SELECT_FISHING_AREA = 'select-fishing-area'
     WAIT_BOBBER = 'wait-bobber'
+    TRACK_BOBBER = 'track-bobber'
+    TRIGGER = 'trigger'
+    START_FISHING = 'start-fishing'
 
 
-def convertQImageToMat(img: QImage) -> np.ndarray: 
+def convertQImageToMat(img: QImage, dims=4) -> np.ndarray: 
     '''  Converts a QImage into an opencv MAT format  '''
     img = img.convertToFormat(4)
+    # if dims == 4:
+    #     img = img.convertToFormat(QtGui.QImage.Format.Format_RGB32)
+    # elif dims == 3:
+    #     img = img.convertToFormat(QtGui.QImage.Format.Format_RGB888)
+    # else:
+    #     raise RuntimeError('invalid image format, only 3,4 channels supported')
     w, h = img.width(), img.height()
     ptr = img.bits()
     ptr.setsize(img.byteCount())
     arr = np.array(ptr).reshape(h, w, 4)
-    return arr
+    if dims == 4:
+        return arr
+    elif dims == 3:
+        arr_out = np.zeros((*arr.shape[:2], 3), np.uint8)
+        arr_out[:,:,0] = arr[:,:,0]
+        arr_out[:,:,1] = arr[:,:,1]
+        arr_out[:,:,2] = arr[:,:,2]
+        return arr_out
+    else:
+        raise RuntimeError(f'unsupported color channel dimension: {dims}')
 
 class MainWindow(QMainWindow):
     def __init__(self, rect = (0, 0 , 220, 32), wow_hwnd = 0, ahk = ahk.AHK()):
         QMainWindow.__init__(self)
-        self.setWindowFlags(
-            #QtCore.Qt.WindowStaysOnTopHint |
-            QtCore.Qt.WindowType.FramelessWindowHint |
-            QtCore.Qt.WindowType.X11BypassWindowManagerHint
-        )
-        self.setWindowOpacity(0.4)
+        # self.setWindowFlags(
+        #     #QtCore.Qt.WindowStaysOnTopHint |
+        #     QtCore.Qt.WindowType.FramelessWindowHint |
+        #     QtCore.Qt.WindowType.X11BypassWindowManagerHint
+        # )
+        self.setWindowOpacity(0.6)
+
+        w = QWidget()
+        self.w = w
+        w.setLayout(QVBoxLayout())
+        self.setCentralWidget(w)
+
         self.hwnd = wow_hwnd
         self.state = WindowState.NORMAL
         r = rect
         self.rect1 = rect
         self.setGeometry(r[0], r[1], r[2], r[3])
-        self.lbl = QtWidgets.QLabel(self)
+        self.lbl = QLabel()
         self.timer = QTimer()
         self.timer.setInterval(120)
         self.timer.timeout.connect(self.update)
         self.timer.start()
         self.timer_timeout_cnt = 0
         self.hist = []
+        self.bhist = []
         self.hist_max = 20
         self.selection = QRect()
-        self.txtStatus = QLabel(self)
+        #self.stack = QVBoxLayout()
+        #self.stack.setDirection(QVBoxLayout.Direction.Down)
+        # self.stack.addWidget(self.windowStatus)
+        # self.stack.addWidget(self.txtStatus)
+        # self.l = QVBoxLayout()
+        # self.l.addChildLayout(self.stack)
+        #self.setLayout(self.stack)
+
+
+        self.txtStatus = QLabel()
         self.txtStatus.setFixedWidth(400)
         self.txtStatus.setStyleSheet("color: #ffffff; background-color: black;")
         self.txtStatus.setText(self.state.name)
+        #self.sp = QSpacerItem(1,1)
+        self.windowStatus = QLabel()
+        self.windowStatus.setFixedWidth(400)
+        self.windowStatus.setStyleSheet("color: #ffffff; background-color: black;")
+        self.setLayoutDirection(QtCore.Qt.LayoutDirection.LayoutDirectionAuto)
+        self.fish_loop_toggle = False
+        self.cvtb = {True: 'Y', False: 'N'}
+        self.fish_loop_toggle_fmt = lambda: f'L:{self.cvtb[self.fish_loop_toggle]}'
+        self.windowStatus.setText(self.fish_loop_toggle_fmt())
+
+        w.layout().addWidget(self.lbl)
+        w.layout().addWidget(self.txtStatus)
+        w.layout().addWidget(self.windowStatus)
+
+        #self.stack.addWidget(self.txtStatus)
+
+        # self.l.addWidget(self.lbl)
         self.rubberBand = QRubberBand(QRubberBand.Shape.Rectangle, self)
         self.origin = QPoint()
         self.ahk = ahk
         self.wow = ahk.find_window(title=b'World of Warcraft')
         self.countdown = millis_now()
         self.coord = None
+        self.pcoord = None
+        self.det = BobberDetector()
+        self.rect = None
+        self.threadpool = QThreadPool()
+        self.mut = QMutex()
+        self.tt = None
+        self.msk = None
+        '''
+        self.setGeometry(100,100, 640, 480)
+        l = QVBoxLayout()
+        self.l3 = QLabel()
+        self.p = QPixmap()
+        self.p.load("after.png")
+        self.p = self.p.scaled(640, 480)
+        self.l3.setPixmap(self.p)
+        self.l3.resize(self.p.size())
+        l.addWidget(self.l3)
+        w = QWidget()
+        w.setLayout(l)
+        self.setCentralWidget(w)
+        self.l1 = QLabel()
+        self.l1.setText("label1")
+        self.l1.setFixedWidth(400)
+        self.l1.setFixedHeight(15)
+        self.l2 = QLabel()
+        self.l2.setText("label2")
+        self.l2.setFixedHeight(15)
+        self.l2.setFixedWidth(400)
+        l.addWidget(self.l1, QtCore.Qt.AlignmentFlag.AlignTop)
+        l.addSpacing(1)
+        l.addWidget(self.l2, QtCore.Qt.AlignmentFlag.AlignTop)
+        l.addStretch(1)
+        '''
         assert self.wow != None
-
 
     def keyPressEvent(self, a0: QtGui.QKeyEvent) -> None:
         if a0.key() == QtCore.Qt.Key.Key_Escape:
-            for i, img in enumerate(self.hist):
-                img.save(f'img{str(i).zfill(2)}.png')
+            # for i, img in enumerate(self.hist):ex
+            #     img.save(f'img{str(i).zfill(2)}.png')
+            self.threadpool.clear()
             QtWidgets.QApplication.instance().exit(0)
         if a0.key() == QtCore.Qt.Key.Key_S:
             if self.state == WindowState.NORMAL:
                 self.state = WindowState.SELECT_FISHING_AREA
                 self.txtStatus.setText(self.state.name)
         if a0.key() == QtCore.Qt.Key.Key_0:
-            # save frame
-            #ahk.key_press()
-            self.hist[-1].save('before.png')
-            self.wow.send('3')
-            self.countdown = millis_now()
-            self.state = WindowState.WAIT_BOBBER
-            # init bobber wait
-            # after a period of time save another frame
-            # find bobber coord
-            pass
+            if not self.selection.isEmpty():
+                self.state = WindowState.START_FISHING
+                self.txtStatus.setText(self.state.name)
+                self.countdown = millis_now()
+            else:
+                print('set up fishing area')
+        if a0.key() == QtCore.Qt.Key.Key_L:
+            self.fish_loop_toggle = not self.fish_loop_toggle
+            self.windowStatus.setText(self.fish_loop_toggle_fmt())
         return super().keyPressEvent(a0)
 
 
@@ -114,6 +200,24 @@ class MainWindow(QMainWindow):
                 #print(self.selection)
                 self.rubberBand.hide()
 
+    def worker_complete(self):
+        self.worker_busy = False
+
+    def worker_handle_result(self, res):
+        if len(res) == 1:
+            with QMutexLocker(self.mut):
+                self.state = WindowState.TRACK_BOBBER
+                rect, conf, l = next(iter(res))
+                self.rect = tuple(rect)
+        else:
+            with QMutexLocker(self.mut):
+                self.state = WindowState.START_FISHING if self.fish_loop_toggle else WindowState.NORMAL
+                self.countdown = millis_now()
+            if not res:
+                print('bobber not found')
+            else:
+                print('many bobbers found')
+
 
     def update(self):
         screen = QtWidgets.QApplication.primaryScreen()
@@ -127,36 +231,154 @@ class MainWindow(QMainWindow):
             while len(self.hist) > self.hist_max:
                 self.hist.pop(0)
 
+            if self.state == WindowState.START_FISHING:
+                if millis_now() - self.countdown > 1000:
+                    self.wow.send('3')
+                    self.countdown = millis_now()
+                    self.state = WindowState.WAIT_BOBBER
+
+            
             if self.state == WindowState.WAIT_BOBBER:
                 if millis_now() - self.countdown > 3000:
                     self.hist[-1].save('after.png')
-                    self.state = WindowState.NORMAL
-
-                    import cv2 as cv
-                    import matplotlib.pyplot as plt
-                    #img = cv.imread('after.png')
-
+                    #self.state = WindowState.NORMAL
                     img = convertQImageToMat(self.hist[-1].toImage())
-                    img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
-                    hls = cv.cvtColor(img, cv.COLOR_RGB2HLS)
-                    hue = hls[:,:,0]
-                    r, mask = cv.threshold(hue, 0, 10, cv.THRESH_OTSU + cv.THRESH_BINARY_INV)
-                    nb_components, output, stats, centroids = cv.connectedComponentsWithStats(mask, connectivity=8)
-                    for i, c in enumerate(centroids):
-                        if stats[i][cv.CC_STAT_AREA] > 400:
-                            cc = tuple(map(int, c))
-                            self.coord = (cc[0] + self.selection.left(), cc[1] + self.selection.top())
-                            c = self.coord
-                            #cv.circle(img, tuple(map(int, c)), radius=7, color=(255,255,255), thickness=1)
-            if self.coord and type(self.coord) == tuple:
-                d = 3
+                    img = img[:,:,:3]
+                    worker = Worker(self.det.detect, img)
+                    #worker = Worker(lambda x: [], img)
+                    worker.signals.result.connect(self.worker_handle_result)
+                    worker.signals.finished.connect(self.worker_complete)
+                    self.worker_busy = True
+                    self.threadpool.start(worker)
+
+ #or self.state == WindowState.TRIGGER:
+            if self.state == WindowState.TRACK_BOBBER:
+                if millis_now() - self.countdown > 3000:
+                    if self.rect and type(self.rect) == tuple:
+                        c = self.rect
+                        qimg = screenshot.copy(QRect( \
+                            self.selection.topLeft() + QPoint(*c[:2]), \
+                            self.selection.topLeft() + QPoint(*c[2:]) + QPoint(0, 30))).toImage()
+                        #qimg.save(f"bobber{self.timer_timeout_cnt}.png")
+                        img = convertQImageToMat(qimg, 3)
+                        #msk, cent, a, _ = (img)
+                        x, y, a, msk = sol(img)
+                        cent = None
+                        if x and y:
+                            cent = [x, y]
+                        if cent != None:
+                            self.bhist.append(cent)
+                            while len(self.bhist) > self.hist_max:
+                                self.bhist.pop(0)
+                            self.pcoord = self.coord
+                            self.coord = tuple(cent)
+                            self.msk = msk
+                            # if a < 900:
+                            #     qimg.save(f"fail{millis_now()}.png")
+                        else:
+                            qimg.save(f"fail{millis_now()}.png")
+                            self.state = WindowState.START_FISHING if self.fish_loop_toggle else WindowState.NORMAL
+                            self.rect = None
+                            self.coord = None
+                            self.pcoord = None
+                            self.bhist.clear()
+                            self.countdown = millis_now()
+                            self.msk = None
+
+                        if self.pcoord != None:
+                            l = (QVector2D(*self.coord) - QVector2D(*self.pcoord)).length()
+                            print(l)
+                            if l > 5.0:
+                                self.state = WindowState.TRIGGER
+                                self.countdown = millis_now()
+                if millis_now() - self.countdown > 20000:
+                    self.state = WindowState.START_FISHING if self.fish_loop_toggle else WindowState.NORMAL
+                    self.rect = None
+                    self.coord = None
+                    self.pcoord = None
+                    self.bhist.clear()
+                    self.countdown = millis_now()
+                    self.msk = None
+
+
+            if self.state == WindowState.TRIGGER:
+                if millis_now() - self.countdown < 20000:
+                    c = win32gui.ClientToScreen(self.hwnd, (0,0))
+                    #print(c)
+                    p = QPoint(*c) + self.selection.topLeft() + QPoint(*self.rect[:2]) + QPoint(*self.bhist[0])
+                    print("caught!!!")
+                    #self.wow.activate()
+                    self.ahk.mouse_move(p.x(), p.y())
+                    self.ahk.right_click()
+                    self.activateWindow()
+                    self.rect = None
+                    self.coord = None
+                    self.pcoord = None
+                    self.bhist.clear()
+                    self.msk = None
+                    self.state = WindowState.START_FISHING if self.fish_loop_toggle else WindowState.NORMAL
+                    self.countdown = millis_now()
+
+            if self.coord != None:
+                p = QPainter()
+                # print(self.msk.shape, self.msk.min(), self.msk.max())
+                img2 = np.zeros((*self.msk.shape, 4), np.uint8)
+                img2[:,:,0] = 0
+                img2[:,:,1] = 0
+                img2[:,:,2] = self.msk
+                img2[:,:,3] = self.msk
+                height, width = self.msk.shape
+                bytesPerLine = 4 * width
+                qImg = QImage(img2.data, width, height, bytesPerLine, QImage.Format_ARGB32)
+                p.begin(self.lbl.pixmap())
+                p.setCompositionMode(QPainter.CompositionMode.CompositionMode_SourceOver);
+                p.drawImage(self.selection.topLeft() + QPoint(*self.rect[:2]), qImg)
+                p.end()
+            if self.rect and type(self.rect) == tuple:
+                if millis_now() - self.countdown < 500 and self.state == WindowState.WAIT_BOBBER:
+                    p = QPainter()
+                    p.begin(self.lbl.pixmap())
+                    p.setPen(QColor(255, 0, 0))
+                    c = self.rect
+                    p.drawRect(QRect( \
+                        self.selection.topLeft() + QPoint(*c[:2]), \
+                        self.selection.topLeft() + QPoint(*c[2:])))
+                    p.end()
+            if self.selection:
                 p = QPainter()
                 p.begin(self.lbl.pixmap())
-                c = self.coord
-                p.setPen(QColor(255, 0, 0))
-                p.drawEllipse(c[0], c[1], d, d)
+                p.setPen(QColor(0x135492))
+                p.drawRect(self.selection)
                 p.end()
 
+
+class WorkerSignals(QObject):
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+
+class Worker(QRunnable):
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+    @pyqtSlot()
+    def run(self):
+        try:
+            result = self.fn(
+                *self.args, **self.kwargs
+            )
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)
+        finally:
+            self.signals.finished.emit()
 
 
 if __name__ == '__main__':
